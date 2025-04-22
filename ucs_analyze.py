19868d335ef5363bb7a0c278141fde45832fae3a
Kioto Hirahara <kioto-hirahara.kh1@mamezou.com>
2025-04-22 13:43:01 +0900

    Implement create_ucs_list()

--- ucs_analyze.py
+++ ucs_analyze.py
@@ -1,40 +1,53 @@
 import sys
+import re
 from pathlib import Path
 import ucs_factory
 
 
-def main(pathname):
+def create_ucs_list(pathname):
+    ucs_list = []
     p = Path(pathname)
     if p.is_file():
-        print(p)
+        # print(p)
         ucs = ucs_factory.create(p)
-        print(ucs)
+        ucs_list.append(ucs)
     elif p.is_dir():
-        ucs_dict = {}
-        actor_map = {}
         for xlfile in p.glob('**/*.xlsx'):
-            ucs = ucs_factory.create(xlfile)
-            ucs_dict[ucs.scenario_id] = ucs
-            for actor in ucs.actors:
-                ucs_list = actor_map.get(actor.name)
-                if ucs_list is None:
-                    actor_map[actor.name] = [ucs]
-                else:
-                    ucs_list.append(ucs)
+            if re.match(r'^~.*', xlfile.stem):
+                # テンポラリファイルはスキップ
+                continue
             print(xlfile)
-
-        # アクター一覧
-        actor_name_list = sorted(actor_map.keys())
-        for actor_name in actor_name_list:
-            # print(actor_name)
-            print(f'{actor_name} ... {len(actor_map.get(actor_name))}件')
-            """
-            for ucs in actor_map.get(actor_name):
-                print(f'    {ucs.scenario_id}')
-            """
-
+            ucs = ucs_factory.create(xlfile)
+            ucs_list.append(ucs)
     else:
         print(f'ERROR: {str(p)}: No such file or directory')
+
+    return ucs_list
+
+
+def main(pathname):
+    ucs_list = create_ucs_list(pathname)
+
+    # アクター一覧
+    actor_set = set()
+    actor_ucss_map = {}
+    for ucs in ucs_list:
+        for actor in ucs.actors:
+            actor_set.add(actor.name)
+            if actor.name in actor_ucss_map:
+                actor_ucss_map[actor.name].append(ucs)
+            else:
+                actor_ucss_map[actor.name] = [ucs]
+
+    # アクター一覧
+    actor_name_list = sorted(list(actor_set))
+    for actor_name in actor_name_list:
+        # print(actor_name)
+        print(f'{actor_name} ... {len(actor_ucss_map.get(actor_name))}件')
+        """
+        for ucs in actor_map.get(actor_name):
+        print(f'    {ucs.scenario_id}')
+        """
 
 
 if __name__ == '__main__':
